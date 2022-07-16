7602661a33da6ae908757625666264585950ef7a
sudha123 <sudhanaukala1997@gmail.com>
2022-07-16 18:19:12 +0530

    variables initialized

---
+++ src/com/bridgelabzGambling2/Gambling2.java
@@ -0,0 +1,11 @@
+package com.bridgelabzGambling2;
+
+public class Gambling2 {
+    static int totalWinnigAmount=0;
+    static int winDaysCount=0;
+    static int lossDaysCount=0;
+    static final int MAX_DAYS=20;
+    static final int EVERY_DAY_STAKE=100;
+
+
+}
