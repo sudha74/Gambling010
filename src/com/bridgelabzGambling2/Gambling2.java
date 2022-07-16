cbf4ca66d58b916f6317a0485bfb68ec6f061265
sudha123 <sudhanaukala1997@gmail.com>
2022-07-16 18:33:17 +0530

    uc4

--- src/com/bridgelabzGambling2/Gambling2.java
+++ src/com/bridgelabzGambling2/Gambling2.java
@@ -1,11 +1,47 @@
 package com.bridgelabzGambling2;
 
 public class Gambling2 {
-    static int totalWinnigAmount=0;
-    static int winDaysCount=0;
-    static int lossDaysCount=0;
-    static final int MAX_DAYS=20;
-    static final int EVERY_DAY_STAKE=100;
+    static int totalWinnigAmount = 0;
+    static int winDaysCount = 0;
+    static int lossDaysCount = 0;
+    static final int MAX_DAYS = 20;
+    static final int EVERY_DAY_STAKE = 100;
+
+    static void Gambler() {
+        for (int day = 1; day < MAX_DAYS; day++) {
+            int dayStake = EVERY_DAY_STAKE;
+            int winningAmount;
+            System.out.println("#day" + day);
+            while (dayStake < 150 && dayStake > 50) {
+                dayStake += doBet(1);
+            }
+            winningAmount = (dayStake - 100);
+            System.out.println("winning Amount = " + winningAmount);
+            if (winningAmount > 0) {
+                totalWinnigAmount = totalWinnigAmount + winningAmount;
+                winDaysCount++;
+            } else {
+                lossDaysCount++;
+                totalWinnigAmount = totalWinnigAmount + winningAmount;
+            }
+        }
+        System.out.println("\ntotal winning Amount " + totalWinnigAmount);
+        System.out.println("won days = " + winDaysCount);
+        System.out.println("loss Days =  " + lossDaysCount);
+    }
 
 
+    static int doBet(int bidPrice) {
+        int random = (int) (Math.random() * 10) % 2;
+        if (random == 1) {
+            return +bidPrice;
+        } else {
+            return -bidPrice;
+        }
+    }
 }
+
+
+
+
+
